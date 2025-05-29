1bbc045bf8121a9ca487894ad12c84ab243410d1
Jimmyjohn23 <jordanmckoy449.com>
2025-05-29 13:35:18 -0400

    Completed Scores homework before ai revisions

--- Homework/Score/main.cpp
+++ Homework/Score/main.cpp
@@ -9,23 +9,43 @@ int main() {
 }
 void analyzeScores() {
     int score = 0;
-    int numScores;
+    int numScores = 0;
     std::vector<int> totalScores;
     double averageScore;
 
     while (score != -1) {
         std::cin >> score;
+
         if (score >= 0 && score <= 100) {
             ++numScores;
             totalScores.push_back(score);
-        } else{
+        }else if (score == -1) {
+            break;
+        }else{
             std::cerr << "Invalid Score";
         }
     }
     int total = 0;
+
     for(int i = 0; i < totalScores.size(); i++){
         total += totalScores[i];
     }
+    
+    std::vector<int> higherThanAverage;
+    
+    for(int i = 0; i < totalScores.size(); i++){
+        if(totalScores[i] >= (total / numScores)){
+            higherThanAverage.push_back(totalScores[i]);
+        }else{
+            continue;
+        }
+    }
+
     std::cout << "Total scores entered: " << numScores << std::endl;
     std::cout << "Average score: " << total / numScores << std::endl;
+    std::cout << "Scores above or equal to average: " << std::endl;
+    
+    for(int i = 0; i < higherThanAverage.size(); i++){
+        std::cout << higherThanAverage[i] << std::endl;
+    }
 }
